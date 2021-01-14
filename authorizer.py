3b80cab47f349dcb2454957b0cd8c8324c663ddb
Guilherme Sette <guilhermesette@outlook.com>
2021-01-14 14:56:59 -0300

    implement hours diff function
    Signed-off-by: Guilherme Sette <guilhermesette@outlook.com>

--- authorizer.py
+++ authorizer.py
@@ -1,32 +1,52 @@
 from account import create_account
 from transaction import create_transaction
+from datetime import datetime
 
 transactions = []
 accounts = []
+
+def add_violation(violation, violation_type):
+  if violation_type == "transaction":
+    transaction_violations.append(violation)
+  elif violation_type == "account":
+    account_violations.append(violation)
 
 def sum_transactions_ammount(transactions):
   if len(transactions) == 0:
       return 0
   return transactions[0].amount + sum_transactions_ammount(transactions[1:])
 
+def subtract_time(string_time_a, string_time_b):
+  time_a = datetime.fromisoformat(string_time_a[:-1])
+  time_b = datetime.fromisoformat(string_time_b[:-1])
+  return (time_b - time_a).seconds
+
+def calculate_limit(available_limit, transactions_amount):
+  return available_limit - transactions_amount
+
 def account_not_itialized(account, transaction):
   if (len(accounts) == 0):
-    transaction_violations.append("account-not-initialized")
+    add_violation("account-not-initialized", "transaction")
 
 def card_not_active(account, transaction):
   if (not(account.active_card)):
-    transaction_violations.append("card-not-active")
+   add_violation("card-not-active", "transaction")
 
 def insufficient_limit(account, transaction):
-  if(account.available_limit - sum_transactions_ammount(transactions) < 0):
-    transaction_violations.append("insufficient-limit")
+  if (calculate_limit(account.available_limit, sum_transactions_ammount(transactions))< 0):
+    add_violation("insufficient-limit", "transaction")
+  else:
+    response['account']['available-limit'] = calculate_limit(account.available_limit, sum_transactions_ammount(transactions))
 
 def high_frequency_small_interval(account, transaction):
-  print("4")
-
+  if len(transactions) >= 3:
+    analyzed_transactions = transactions[:3]
+    if (subtract_time(analyzed_transactions[0].time, analyzed_transactions[-1].time) <= 180):
+      add_violation("high-frequency-small-interval", "transaction")
+    
 def account_already_initialized(account, transaction):
   if (len(accounts) > 0):
-    account_violations.append("account-already-initialized")
+    add_violation("account-already-initialized", "account")
 
 def iterates_through_criterias(criterias, account, transaction=""):
   aux_criterias = criterias.copy()
@@ -50,7 +70,7 @@ account_criterias_to_be_applied = {
 response = {
   'account': {
     'active-card': "",
-    'available-limit': ""
+    'available-limit': 0
   },
   'violations': []
 }
@@ -58,7 +78,7 @@ response = {
 def execute_transaction(param):
   global transaction_violations
   transaction_violations = []
-  transaction = create_transaction(param["transaction"]["merchant"], param["transaction"]["amount"], param["transaction"]["amount"])
+  transaction = create_transaction(param["transaction"]["merchant"], param["transaction"]["amount"], param["transaction"]["time"])
   validate_transaction(accounts[0], transaction)
   print(response)
   
@@ -73,7 +93,7 @@ def execute_account(param):
 
 def build_response(account, violations):
   response["account"]["active-card"] = account.active_card
-  response["account"]["available-limit"] = account.available_limit
+  response["account"]["available-limit"] = account.available_limit if(response["account"]["available-limit"]) == 0 else response["account"]["available-limit"]
   response["violations"] = violations
 
 def validate_account(account):
@@ -90,18 +110,20 @@ def validate_transaction(account, transaction):
 
 
     
-
-
-
 param = {"account": {"active-card": False, "available-limit": 30}}
 
 param2 = {"transaction": {"merchant": "Burger King", "amount": 20, "time":
 "2019-02-13T10:00:00.000Z"}}
+param3 = {"transaction": {"merchant": "Burger King", "amount": 20, "time":
+"2019-02-13T10:01:00.000Z"}}
+param4 = {"transaction": {"merchant": "Burger King", "amount": 20, "time":
+"2019-02-13T10:02:00.000Z"}}
+
 
 execute_account(param)
 execute_transaction(param2)
-execute_account(param)
-execute_transaction(param2)
+execute_transaction(param3)
+execute_transaction(param4)
 
     
 
